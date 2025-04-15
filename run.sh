dfb6d0ce514c92b9ffd2f93fb29fc455d664d6b0
Mateusz <m@m.m>
2025-04-15 17:51:00 +0200

    added 12 to .sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 BINARY_DIR="./binary"
 PROGRAMS=("openmp_pierwsze" "mpi_pierwsze" "hybryda_pierwsze")
 ROZMIARY_DANYCH=(4000000 8000000 16000000 32000000 64000000)
-WATKI_PROCESY=(1 2 4 8 16)
+WATKI_PROCESY=(1 2 4 8 12 16)
 
 echo "🔁 Benchmark: wszystkie wersje z różnymi rozmiarami i konfiguracjami"
 echo
